750d59bd989e399171e105fbfa4d988022b6741e
humbercredpay <humberto.almeida@credpay.com.br>
2020-02-11 19:12:26 -0300

    update

---
+++ JimHalpert.Repository/ServicoRepository.cs
@@ -0,0 +1,24 @@
+﻿
+using Dapper;
+using JimHalpert.Domain.Entity;
+using JimHalpert.Domain.Inteface.Repository;
+using JimHalpert.Repository.Interfaces;
+using System.Collections.Generic;
+
+namespace JimHalpert.Repository
+{
+    public class ServicoRepository : BaseRepository<Servico>, IServicoRepository
+    {
+        public ServicoRepository(IContextManager contextManager) : base(contextManager)
+        {
+
+        }
+
+        public IEnumerable<Servico> Filtrar(string query)
+        {
+            var sql = $@"SELECT * FROM Servico WHERE Descricao like '%{query}%'";
+
+            return this.Connection.Query<Servico>(sql);
+        }
+    }
+}
