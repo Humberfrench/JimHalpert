1e2a1a8fd76d88ff930adbc8a08ab2b7bcdbe131
humbercredpay <humberto.almeida@credpay.com.br>
2020-02-11 13:03:57 -0300

    ajustes e implementacoes

---
+++ JimHalpert.Application/Interface/IServicoServiceApp.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq.Expressions;
+using JimHalpert.Application.ViewModel;
+using JimHalpert.DomainValidator;
+
+namespace JimHalpert.Application.Interface
+{
+    public interface IServicoServiceApp
+    {
+        IEnumerable<ServicoViewModel> ObterTodos();
+        IEnumerable<ServicoViewModel> Filtrar(string query);
+        ValidationResult Gravar(ServicoViewModel Servico);
+        ValidationResult Excluir(int id);
+        ServicoViewModel ObterPorId(int id);
+    }
+}
