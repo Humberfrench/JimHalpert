5d4b5500c59eb5abd4c159d34cbbad09b456ab9f
humbercredpay <humberto.almeida@credpay.com.br>
2020-02-17 18:57:24 -0300

    impl js

--- JimHalpert.WebSpa/ClientApp/src/app/servicos/servicos.component.ts
+++ JimHalpert.WebSpa/ClientApp/src/app/servicos/servicos.component.ts
@@ -12,10 +12,26 @@ export class ServicosComponent
 {
   uri: string  = 'http://localhost:56879/';
   clientApi: HttpClient ;
+  public servicos: Servicos[];
 
   constructor(client: HttpClient)
   {
     this.clientApi = client;
-  }
+    this.ObterServicos();
 
+  }
+  ObterServicos()
+  {
+    this.clientApi.get<Servicos[]>(this.uri + 'servicos').subscribe(result =>
+    {
+      this.servicos = result;
+    }, error => console.error(error));
+
+  }
+}
+
+interface Servicos
+{
+  ServicoId: number;
+  Descricao: string;
 }
