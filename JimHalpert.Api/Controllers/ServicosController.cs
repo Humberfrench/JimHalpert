f32ebb6cc346c6246ef6e5ce9366bf47b94a6984
humbercredpay <humberto.almeida@credpay.com.br>
2020-02-19 18:26:23 -0300

    ajustes controller

--- JimHalpert.Api/Controllers/ServicosController.cs
+++ JimHalpert.Api/Controllers/ServicosController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using JimHalpert.Application.Interface;
 using JimHalpert.Application.ViewModel;
+using JimHalpert.DomainValidator;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -25,5 +26,22 @@ namespace JimHalpert.Api.Controllers
         {
             return servicoServiceApp.ObterTodos().ToList();
         }
+        [HttpGet("{id}")]
+        public ServicoViewModel Obter(int id)
+        {
+            return servicoServiceApp.ObterPorId(id);
+        }
+
+        [HttpPost("Excluir/{id}")]
+        public ValidationResult Excluir(int id)
+        {
+            return servicoServiceApp.Excluir(id);
+        }
+        [HttpPost("Gravar")]
+        public ValidationResult Gravar(ServicoViewModel servico)
+        {
+            return servicoServiceApp.Gravar(servico);
+        }
+
     }
 }
