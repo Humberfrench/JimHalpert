1e586ab6a2539ffb364fd633a38502b330858bf2
Humberto Almeida <humberto.almeida@credpay.com.br>
2024-03-08 17:07:20 -0300

    web imple

--- JimHalpert.Web/Controllers/CadastrosController.cs
+++ JimHalpert.Web/Controllers/CadastrosController.cs
@@ -1,7 +1,9 @@
 ﻿using JimHalpert.App.ViewModel;
 using JimHalpert.App.ViewModel.Interface;
 using JimHalpert.Web.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections;
 
 namespace JimHalpert.Web.Controllers
 {
@@ -9,6 +11,8 @@ namespace JimHalpert.Web.Controllers
     public class CadastrosController : Controller
     {
         //Cadastros de tipo de clientes, tipo de pessoas 
+        //https://localhost:44311/Cadastros/Tipo/Pessoa
+
         private readonly ITipoDeClienteServiceApp tipoDeClienteServiceApp;
         private readonly ITipoDePessoaServiceApp tipoDePessoaServiceApp;
 
@@ -21,67 +25,67 @@ namespace JimHalpert.Web.Controllers
 
         #region Tipo de Pessoas
 
-        [HttpGet("Tipo/Pessoa")]
+        [HttpGet("Tipo/Pessoa"),AllowAnonymous]
         public async Task<IActionResult> IndexTipoPessoa()
         {
             var model = new ModelViewItem<List<TipoDePessoaViewModel>>();
 
             model.Item = (await tipoDePessoaServiceApp.ObterTodos()).Content;
 
-            return View("TipoPessoa");
+            return View("TipoPessoa", model);
         }
 
-        [HttpGet("Tipo/Pessoa/New")]
+        [HttpGet("Tipo/Pessoa/New"),AllowAnonymous]
         public async Task<IActionResult> IndexTipoPessoaNew()
         {
             var model = new ModelViewItem<TipoDePessoaViewModel>();
 
             model.Item = new TipoDePessoaViewModel();
 
-            return View("TipoPessoaEdit");
+            return View("TipoPessoaEdit", model);
         }
 
-        [HttpGet("Tipo/Pessoa/Edit/{id}")]
+        [HttpGet("Tipo/Pessoa/Edit/{id}"),AllowAnonymous]
         public async Task<IActionResult> IndexTipoPessoaEdit(int id)
         {
             var model = new ModelViewItem<TipoDePessoaViewModel>();
 
             model.Item = (await tipoDePessoaServiceApp.ObterPorId(id)).Content;
 
-            return View("TipoPessoaEdit");
+            return View("TipoPessoaEdit", model);
         }
         #endregion
 
         #region Tipo de Pessoas
 
-        [HttpGet("Tipo/Cliente")]
+        [HttpGet("Tipo/Cliente"),AllowAnonymous]
         public async Task<IActionResult> IndexTipoCliente()
         {
             var model = new ModelViewItem<List<TipoDeClienteViewModel>>();
 
             model.Item = (await tipoDeClienteServiceApp.ObterTodos()).Content; 
 
-            return View("TipoCliente");
+            return View("TipoCliente", model);
         }
 
-        [HttpGet("Tipo/Cliente/New")]
+        [HttpGet("Tipo/Cliente/New"),AllowAnonymous]
         public async Task<IActionResult> IndexTipoClienteNew()
         {
             var model = new ModelViewItem<TipoDeClienteViewModel>();
 
             model.Item = new TipoDeClienteViewModel();
 
-            return View("TipoClienteEdit");
+            return View("TipoClienteEdit", model);
         }
 
-        [HttpGet("Tipo/Cliente/Edit/{id}")]
+        [HttpGet("Tipo/Cliente/Edit/{id}"),AllowAnonymous]
         public async Task<IActionResult> IndexTipoClienteEdit(int id)
         {
             var model = new ModelViewItem<TipoDeClienteViewModel>();
 
             model.Item = (await tipoDeClienteServiceApp.ObterPorId(id)).Content;
 
-            return View("TipoClienteEdit");
+            return View("TipoClienteEdit", model);
         }
         #endregion
 
